99751f8347b32391fb1a364d2948ae3f6a2ee7fb
Sam Sweeney <samsweeneyy@gmail.com>
2023-03-28 12:09:22 +1100

    Added User model

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", UserSchema);
